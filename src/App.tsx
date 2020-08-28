5acf113aea269f5e9696f5aa04f2f02b7ac9b26b
allexis096 <allexissoares@gmail.com>
2020-08-28 20:40:24 -0300

    :sparkles: feat: create a Dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+import Dashboard from './pages/Dashboard';
+
+const App: React.FC = () => {
+  return <Dashboard />;
+};
 
 export default App;
